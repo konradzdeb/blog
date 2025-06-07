d2fd844e2e59ba26767958c91d9100b3b4fc432f
Konrad Zdeb <konrad.zdeb@me.com>
2025-06-07 14:42:12 +0100

    Progressing article
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

--- content/post/2025-05-02-using-python-model-in-swift/temp_fetched_code.py
+++ content/post/2025-05-02-using-python-model-in-swift/temp_fetched_code.py
@@ -1,10 +1,11 @@
 """Example model training solution."""
 
-from sklearn.datasets import load_iris  # noqa: E0401
+from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 
 # Load data
 iris = load_iris()
+
 X, y = iris.data, iris.target
